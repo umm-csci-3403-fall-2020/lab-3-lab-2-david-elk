2ac88a699efc12e500fa32a8ffd112e162aed2da
david <lucht013@morris.umn.edu>
2020-09-19 17:50:44 -0500

    Add some code for array_merge

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,6 +1,13 @@
 #include "array_merge.h"
+#include "stdio.h"
+#include "stdbool.h"
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
   // This is obviously broken. It has the right type, though, eh?
+  for(i = 0; i < num_arrays; i++){
+  	for(j = 0; j<sizes[i]; j++){
+  		printf(values[i][j]);
+  	}
+  }
   return sizes;
 }
---
+++ array_merge/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <array_merge.h>
+
+int main(int argc, char *argv[]){
+  
+
+}
