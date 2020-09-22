0acea78d05b3342865375aebae63d04900625d71
david <lucht013@morris.umn.edu>
2020-09-22 11:33:10 -0500

    Add hopefully working array_merge

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,13 +1,52 @@
+#include "../mergesort/mergesort.h"
 #include "array_merge.h"
-#include "stdio.h"
-#include "stdbool.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool isUnique(int n, int indexCounter, int* tempArray){
+  bool unique = true;
+  int i;
+  for(i = 0; i < indexCounter; i++){
+	if(n == tempArray[i]){
+  	  unique = false;
+  	  return unique;
+  	}
+  	
+  }
+  return unique;
+}
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
-  // This is obviously broken. It has the right type, though, eh?
+  int i;
+  int j;
+  int k;
+  int l;
+  int tempCounter = 0;
+
+  for(k = 0; k < num_arrays; i++ ){
+  	tempCounter = tempCounter + sizes[k];
+  }
+
+  int *tempArray = (int*) calloc(tempCounter, sizeof(int));
+  
+  int indexCounter = 0;
   for(i = 0; i < num_arrays; i++){
   	for(j = 0; j<sizes[i]; j++){
-  		printf(values[i][j]);
+  		if(isUnique(values[i][j],indexCounter,tempArray)){
+  			tempArray[indexCounter] = values[i][j];
+  			indexCounter++;
+  		}
   	}
   }
-  return sizes;
+
+  int *finalArray = (int*) calloc(indexCounter + 1,sizeof(int));
+  finalArray[0] = 0;
+  for(l = 0; l < indexCounter; l++){
+  	finalArray[l+1] = tempArray[l];
+  }
+  free(tempArray);
+  mergesort(indexCounter + 1, finalArray);
+  finalArray[0] = indexCounter;
+  return finalArray;
 }
--- array_merge/array_merge_test.cpp
+++ array_merge/array_merge_test.cpp
@@ -18,6 +18,7 @@ TEST(ArrayMerge, Handle_empty_list) {
 
   result = array_merge(0, sizes,  a);
   arrays_match(1, result, expected);
+  free(result);
 }
 
 TEST(ArrayMerge, Handle_singleton_list) {
@@ -30,6 +31,7 @@ TEST(ArrayMerge, Handle_singleton_list) {
 
   result = array_merge(num_arrays, sizes, a);
   arrays_match(2, result, expected);
+  free(result);
 }
 
 TEST(ArrayMerge, Handle_one_longer_list) {
@@ -42,6 +44,7 @@ TEST(ArrayMerge, Handle_one_longer_list) {
 
   result = array_merge(num_arrays, sizes, a);
   arrays_match(8, result, expected);
+  free(result);
 }
 
 TEST(ArrayMerge, Handle_multiple_copies_of_longer_list) {
@@ -54,6 +57,7 @@ TEST(ArrayMerge, Handle_multiple_copies_of_longer_list) {
 
   result = array_merge(num_arrays, sizes, a);
   arrays_match(8, result, expected);
+  free(result);
 }
 
 TEST(ArrayMerge, Handle_multiple_copies_of_longer_list_different_orders) {
@@ -68,6 +72,7 @@ TEST(ArrayMerge, Handle_multiple_copies_of_longer_list_different_orders) {
 
   result = array_merge(num_arrays, sizes, a);
   arrays_match(8, result, expected);
+  free(result);
 }
 
 TEST(ArrayMerge, Handle_different_sizes) {
@@ -88,6 +93,7 @@ TEST(ArrayMerge, Handle_different_sizes) {
 
   result = array_merge(num_arrays, sizes, a);
   arrays_match(11, result, expected);
+  free(result);
 }
 
 TEST(ArrayMerge, Handle_different_sizes_reversed) {
@@ -108,6 +114,7 @@ TEST(ArrayMerge, Handle_different_sizes_reversed) {
 
   result = array_merge(num_arrays, sizes, a);
   arrays_match(11, result, expected);
+  free(result);
 }
 
 int main(int argc, char* argv[]) {
